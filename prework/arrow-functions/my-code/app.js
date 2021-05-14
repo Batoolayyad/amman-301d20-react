8b7258cc0cbd8c97e8735518553a28f56fa274a9
Batoolayyad <batoolayyad1996@gmail.com>
2021-05-14 18:51:53 +0300

    finis lab

--- prework/arrow-functions/my-code/app.js
+++ prework/arrow-functions/my-code/app.js
@@ -223,8 +223,8 @@ console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-// undefined 
+// undefined .
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//undefined
+//undefined .
 // 3. Explain why "this" is different when an arrow function is used.
 //
